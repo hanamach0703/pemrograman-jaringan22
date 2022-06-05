488c982c2d33500f7f80415912cc555de858144c
hanamach0703 <machmudahhana@gmail.com>
2022-06-05 22:40:02 +0700

    POST and DELETE

--- Tugas 1/file_client_cli.py
+++ Tugas 1/file_client_cli.py
@@ -2,8 +2,10 @@ import socket
 import json
 import base64
 import logging
+import os
 
-server_address=('0.0.0.0',7777)
+server_address = ('0.0.0.0', 7777)
+
 
 def send_command(command_str=""):
     global server_address
@@ -12,14 +14,15 @@ def send_command(command_str=""):
     logging.warning(f"connecting to {server_address}")
     try:
         logging.warning(f"sending message ")
+        command_str += "\r\n\r\n"  # tambahan
         sock.sendall(command_str.encode())
         # Look for the response, waiting until socket is done (no more data)
-        data_received="" #empty string
+        data_received = ""  # empty string
         while True:
-            #socket does not receive all data at once, data comes in part, need to be concatenated at the end of process
+            # socket does not receive all data at once, data comes in part, need to be concatenated at the end of process
             data = sock.recv(16)
             if data:
-                #data is not empty, concat with previous content
+                # data is not empty, concat with previous content
                 data_received += data.decode()
                 if "\r\n\r\n" in data_received:
                     break
@@ -37,35 +40,71 @@ def send_command(command_str=""):
 
 
 def remote_list():
-    command_str=f"LIST"
+    command_str = f"LIST"
     hasil = send_command(command_str)
-    if (hasil['status']=='OK'):
+    if hasil['status'] == 'OK':
         print("daftar file : ")
         for nmfile in hasil['data']:
             print(f"- {nmfile}")
         return True
     else:
-        print("Gagal")
+        print("Gagal Mendapatkan List File")
         return False
 
+
 def remote_get(filename=""):
-    command_str=f"GET {filename}"
+    command_str = f"GET {filename}"
     hasil = send_command(command_str)
-    if (hasil['status']=='OK'):
-        #proses file dalam bentuk base64 ke bentuk bytes
-        namafile= hasil['data_namafile']
+    if hasil['status'] == 'OK':
+        # proses file dalam bentuk base64 ke bentuk bytes
+        namafile = hasil['data_namafile']
         isifile = base64.b64decode(hasil['data_file'])
-        fp = open(namafile,'wb+')
+        fp = open(namafile, 'wb+')
         fp.write(isifile)
         fp.close()
         return True
     else:
-        print("Gagal")
+        print("Gagal Mendapatkan File")
         return False
 
 
-if __name__=='__main__':
-    server_address=('172.16.16.101',6666)
-    remote_list()
-    remote_get('donalbebek.jpg')
+# fungsi mengupload foto dalam folder file
+def remote_post(filename=""):
+    if not os.path.exists(filename):
+        print(f"Gagal: file{filename} tidak ditemukan")
+        return False
 
+    fp = open(f"{filename}", 'rb')
+    filecontent = base64.b64encode(fp.read()).decode()
+
+    command_str = f"POST {filename} {filecontent}"
+    hasil = send_command(command_str)
+    print(hasil['data'])
+
+    if hasil['status'] == 'OK':
+        return True
+    else:
+        print("Gagal Mengupload File")
+        return False
+
+
+# fungsi menghapus foto dalam folder file
+def remote_delete(filename=""):
+    command_str = f"DELETE {filename}"
+    hasil = send_command(command_str)
+    print(hasil['data'])
+
+    if hasil['status'] == "OK":
+        return True
+    else:
+        print("Gagal Menghapus File")
+        return False
+
+
+if __name__ == '__main__':
+    server_address = ('172.16.16.102', 6666)
+    print(os.path)
+    remote_list()
+    remote_get('pokijan.jpg')
+    remote_delete('dora_delete.jpg')
+    remote_post('pororo_upload.jpg')
--- Tugas 1/file_interface.py
+++ Tugas 1/file_interface.py
@@ -8,27 +8,58 @@ class FileInterface:
     def __init__(self):
         os.chdir('files/')
 
-    def list(self,params=[]):
+    def list(self, params=[]):
         try:
             filelist = glob('*.*')
-            return dict(status='OK',data=filelist)
+            return dict(status='OK', data=filelist)
         except Exception as e:
-            return dict(status='ERROR',data=str(e))
+            return dict(status='ERROR', data=str(e))
 
-    def get(self,params=[]):
+    def get(self, params=[]):
         try:
             filename = params[0]
             if (filename == ''):
                 return None
-            fp = open(f"{filename}",'rb')
+            fp = open(f"{filename}", 'rb')
             isifile = base64.b64encode(fp.read()).decode()
-            return dict(status='OK',data_namafile=filename,data_file=isifile)
+            return dict(status='OK', data_namafile=filename, data_file=isifile)
         except Exception as e:
-            return dict(status='ERROR',data=str(e))
+            return dict(status='ERROR', data=str(e))
+
+    # tambah fungsi untuk upload
+    def post(self, params=[]):
+        if not (len(params) == 2):
+            return dict(status='ERROR')
+
+        # cek file yang sama
+        filename = params[0]
+        if os.path.exists(filename):
+            return dict(status='ERROR', data=f'{filename} file sudah ada dalam folder')
+
+        file = base64.b64decode(params[1])
+        fp = open(filename, 'wb+')
+        fp.write(file)
+        fp.close()
+        return dict(status='OK', data=f'file {filename} berhasil diupload')
+
+    # tambah fungsi untuk delete
+    def delete(self, params=[]):
+        if not (len(params) == 1):
+            return dict(status='ERROR')
+
+        filename = params[0]
+        if not os.path.exists(filename):
+            return dict(status='ERROR')
+
+        os.remove(filename)
+        return dict(status='OK', data=f'file {filename} berhasil didelete')
+
+    def version(self, params=[]):
+        return dict(status='OK', data='versi 1.0')
 
 
-
-if __name__=='__main__':
+if __name__ == '__main__':
     f = FileInterface()
+    print(f.version())
     print(f.list())
     print(f.get(['pokijan.jpg']))
